13ae9bc7abf9c6d924c85c1d2ea8eced84f5cbec
MotasemZiad <miziad2000@gmail.com>
2022-07-21 18:17:41 +0300

    some udpates

---
+++ 10_python_package_index/main.py
@@ -0,0 +1,24 @@
+# Python Package Index (PPI)
+# Some pip useful commands to use
+# ! Semantic version: 2.28.1 => major.minor.bug_fixes
+# ? pip install package_name
+# ? pip install --upgrade pip # the latest version of pip
+# ? pip install --upgrade package_name # To upgrade the package to the latest version
+# ? pip install package_name==2.9.0
+# ? pip install package_name==2.9.*
+# ? pip uninstall package_name
+# ? pip download package_name
+# ? pip search package_name
+
+import requests
+
+get_users = requests.get(url="https://jsonplaceholder.typicode.com/users")
+print(get_users.status_code)
+print(get_users.headers['Content-Type'])
+print(get_users.encoding)
+print(get_users.text)
+print(get_users.json())
+
+
+# Virtual Environment -> Docker, kubernetes, etc...
+# Create an isolated virtual environment and install the dependencies you want.
