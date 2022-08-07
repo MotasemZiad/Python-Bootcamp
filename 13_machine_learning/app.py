ab46a43e31a88b4e5de630cbf5e072f0648a59cc
MotasemZiad <miziad2000@gmail.com>
2022-08-07 13:37:38 +0300

    some updates

--- 13_machine_learning/app.py
+++ 13_machine_learning/app.py
@@ -1,1 +1,13 @@
 print("Machine Learning with Python")
+# Steps of Machine Learning Programs:
+# 1. Import the Data.
+# 2. Clean the Data.
+# 3. Split the Data into Training/Test Sets. (80% for training 20% for testing).
+# 4. Create a Model.
+# 5. Train the Model.
+# 6. Make Predictions.
+# 7. Evaluate and Improve.
+
+# Libraries and tools:
+# scikit learn, tenserflow, pytorch, keras, numpy, pandas, scipy, matplotlib, etc.
+# jupyter, anaconda, etc. 
