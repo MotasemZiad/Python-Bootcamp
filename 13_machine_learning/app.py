dd9bfcb4e84e5c78e4fba0ad0141fa21505999f7
MotasemZiad <miziad2000@gmail.com>
2022-08-08 17:29:26 +0300

    finish machine learning module

--- 13_machine_learning/app.py
+++ 13_machine_learning/app.py
@@ -9,5 +9,40 @@ print("Machine Learning with Python")
 # 7. Evaluate and Improve.
 
 # Libraries and tools:
-# scikit learn, tenserflow, pytorch, keras, numpy, pandas, scipy, matplotlib, etc.
-# jupyter, anaconda, etc. 
+# scikit learn, tenserflow, pytorch, keras, pandas, numpy, scipy, matplotlib, etc.
+# jupyter, anaconda, etc.
+
+
+# Jupyter Notebook:
+# import pandas as pd
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import accuracy_score
+# import joblib
+
+# music_data_frame = pd.read_csv('music.csv')
+
+# # input data set & output data set
+# X = music_data_frame.drop(columns= ['genre'])
+# y = music_data_frame['genre']
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+
+# model = DecisionTreeClassifier()
+# model.fit(X_train, y_train)
+
+# # Persisting Models
+# joblib.dump(model, 'music-recommender.joblib')
+
+# predictions = model.predict(X_test)
+# score = accuracy_score(y_test, predictions)
+# score
+
+# Persisting Models
+# import joblib
+
+# model = joblib.load('music-recommender.joblib')
+
+# predictions = model.predict([ [21, 1], [22, 2]])
+# predictions
+# # score = accuracy_score(y_test, predictions)
+# # score
