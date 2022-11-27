0fad32793b1f964eca9729d1d686d6b339cb2760
MotasemZiad <miziad2000@gmail.com>
2022-11-27 12:38:50 +0200

    clarifying some methods

--- advanced_python/types_and_typehints.py
+++ advanced_python/types_and_typehints.py
@@ -1,6 +1,24 @@
+from typing import Callable
+
+IntFunction = Callable[[int], int]
+
+
+def multiply_by_two(x: float) -> float:
+    return x * 2.0
+
+
+def add_three(x: int) -> int:
+    return x + 3
+
+
+def compute_stats(users, plans, products):
+    # some complicated code..
+    pass
+
+
 def main():
-    my_var = "hello"
-    print(my_var)
+    my_var: IntFunction = multiply_by_two
+    print(my_var(5))
 
 
 if __name__ == '__main__':
