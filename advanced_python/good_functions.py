51799d6bff768b16a1909f849ca37708bbe0e850
MotasemZiad <miziad2000@gmail.com>
2022-12-04 16:39:28 +0200

    writing methods...

--- advanced_python/good_functions.py
+++ advanced_python/good_functions.py
@@ -1,7 +1,17 @@
-# The ultimate guide to write a good function in python.
+""" The ultimate guide to write a good function in python:
+    1. Do one thing and do it well.
+    2. Separate commands from queries.
+    3. Only request information you actually need.
+    4. Keep the number of parameters minimal.
+    5. Don't create and use an object in the same place.
+    6. Don't use flag arguments.
+    7. Remember that functions are object.
+    Bonus: Follow the naming convention and the style guide of your language. 
+"""
+
 
 def main():
-    print("The ultimate guide to write a good function in python.")
+    print("Hello, Python...")
 
 
 if __name__ == "__main__":
