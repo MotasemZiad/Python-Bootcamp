4134d5ca4c98e7c8cd57d4ffda9d1ed07db2896d
MotasemZiad <miziad2000@gmail.com>
2022-12-04 16:21:49 +0200

    initials "write clean functions in python"

---
+++ advanced_python/good_functions.py
@@ -0,0 +1,8 @@
+# The ultimate guide to write a good function in python.
+
+def main():
+    print("The ultimate guide to write a good function in python.")
+
+
+if __name__ == "__main__":
+    main()
