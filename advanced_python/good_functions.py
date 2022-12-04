907039b85e20994219ed4657727647d62b5ed3b7
MotasemZiad <miziad2000@gmail.com>
2022-12-04 17:13:23 +0200

    extract one method into two methods

--- advanced_python/good_functions.py
+++ advanced_python/good_functions.py
@@ -23,19 +23,23 @@ class Customer:
     cc_valid: bool = False
 
 
-def validate_card(customer: Customer) -> bool:
+def luhn_checksum(card_number: str) -> bool:
     def digits_of(number: str) -> list[int]:
         return [int(d) for d in number]
 
-    digits = digits_of(customer.cc_number)
+    digits = digits_of(card_number)
     odd_digits = digits[-1::-2]
     even_digits = digits[-2::-2]
     checksum = 0
     checksum += sum(odd_digits)
     for digit in even_digits:
         checksum += sum(digits_of(str(digit * 2)))
+    return checksum % 10 == 0
+
+
+def validate_card(customer: Customer) -> bool:
     customer.cc_valid = (
-        checksum % 10 == 0 and datetime(
+        luhn_checksum(customer.cc_number) and datetime(
             customer.cc_exp_year, customer.cc_exp_month, 1) > datetime.now()
     )
 
