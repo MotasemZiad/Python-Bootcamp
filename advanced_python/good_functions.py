0bc5314077923f275701e5bcc432fa2633969dac
MotasemZiad <miziad2000@gmail.com>
2022-12-04 17:09:06 +0200

    code smell (violate single responsibility principle)

--- advanced_python/good_functions.py
+++ advanced_python/good_functions.py
@@ -1,5 +1,5 @@
 """ The ultimate guide to write a good function in python:
-    1. Do one thing and do it well.
+    1. Do one thing, and do it well.
     2. Separate commands from queries.
     3. Only request information you actually need.
     4. Keep the number of parameters minimal.
@@ -9,9 +9,50 @@
     Bonus: Follow the naming convention and the style guide of your language. 
 """
 
+from dataclasses import dataclass
+from datetime import datetime
 
-def main():
-    print("Hello, Python...")
+
+@dataclass
+class Customer:
+    name: str
+    phone: str
+    cc_number: str
+    cc_exp_month: int
+    cc_exp_year: int
+    cc_valid: bool = False
+
+
+def validate_card(customer: Customer) -> bool:
+    def digits_of(number: str) -> list[int]:
+        return [int(d) for d in number]
+
+    digits = digits_of(customer.cc_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    checksum = 0
+    checksum += sum(odd_digits)
+    for digit in even_digits:
+        checksum += sum(digits_of(str(digit * 2)))
+    customer.cc_valid = (
+        checksum % 10 == 0 and datetime(
+            customer.cc_exp_year, customer.cc_exp_month, 1) > datetime.now()
+    )
+
+    return customer.cc_valid
+
+
+def main() -> None:
+    ahmed = Customer(
+        name="Ahmed Ali",
+        phone="+00970598926087",
+        cc_number="1222093487125099",
+        cc_exp_month=1,
+        cc_exp_year=2025,
+    )
+    is_valid = validate_card(ahmed)
+    print(f"Is Ahmed's card valid? {is_valid}")
+    print(ahmed)
 
 
 if __name__ == "__main__":
